a8f4d106f5b9cd3702b390096b644b7af8f3cc19
LaithLou <laithlouay@gmail.com>
2022-06-30 15:27:58 -0400

    added post and put

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 "use strict";
-
+//kungpaodao:foofoo@localhost
 /** Database setup for BizTime. */
 
 const { Client } = require("pg");
 
-const DB_URI = process.env.NODE_ENV === "test"
+const DB_URI =
+  process.env.NODE_ENV === "test"
     ? "postgresql:///kungpaodao:foofoo@localhost/biztime_test"
-    : "postgresql://kungpaodao:foofoo@localhost/biztime";
+    : "postgresql:///biztime";
 
 let db = new Client({
-  connectionString: DB_URI
+  connectionString: DB_URI,
 });
 
 db.connect();
--- routes/companies.js
+++ routes/companies.js
@@ -1,68 +1,67 @@
 "use strict";
 
-const express = require('express');
+const express = require("express");
 const db = require("../db");
 
 const router = new express.Router();
 
 /** Returns a list of companies with all their information:
- *  
+ *
  * {companies : [
  *  {"code": "apple", "name": "Apple Computer", "description": "Maker of OSX."},
 	{"code": "ibm","name": "IBM","description": "Big blue."}
     ]}
- * 
+ *
  */
 
-router.get("/", async function (req, res){
-    const result = await db.query(
-        `SELECT code, name, description 
-        FROM companies`);
-    const companies = result.rows;
-    return res.json({ "companies" : companies });
+router.get("/", async function (req, res) {
+  const result = await db.query(
+    `SELECT code, name, description
+        FROM companies`
+  );
+  const companies = result.rows;
+  return res.json({ companies: companies });
 });
 
-router.get("/:code", async function (req, res){
-    const code = req.params.code;
-    const result = await db.query(
-        `SELECT code, name, description 
-        FROM companies 
-        WHERE code = $1`, [code]);
-    const company = result.rows[0];
-    return res.json({ company })
-})
+router.get("/:code", async function (req, res) {
+  const code = req.params.code;
+  const result = await db.query(
+    `SELECT code, name, description
+        FROM companies
+        WHERE code = $1`,
+    [code]
+  );
+  const company = result.rows[0];
+  return res.json({ company });
+});
 
+router.post("/", async function (req, res) {
+  const { code, name, description } = req.body;
 
+  const result = await db.query(
+    `INSERT INTO companies (code,name,description)
+        VALUES ($1,$2,$3)
+        RETURNING code,name,description`,
+    [code, name, description]
+  );
+  const company = result.rows[0];
+  return res.status(201).json({ company });
+});
 
+module.exports = router;
 
+router.put("/:code", async function (req, res) {
+  const { code, name, description } = req.body;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const result = await db.query(
+    `UPDATE companies
+          SET name = $1, description = $2
+          WHERE code = $3
+          RETURNING code,name,description`,
+    [name, description, req.params.code]
+  );
+  const company = result.rows[0];
+  return res.json({ company });
+});
 
 module.exports = router;
